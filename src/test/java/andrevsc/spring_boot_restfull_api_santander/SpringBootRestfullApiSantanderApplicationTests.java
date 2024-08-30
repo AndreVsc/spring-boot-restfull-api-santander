8b1d24f85b33d0aee4a719d3b3ba9475052cf837
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-30 08:14:13 -0300

    add project

---
+++ src/test/java/andrevsc/spring_boot_restfull_api_santander/SpringBootRestfullApiSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package andrevsc.spring_boot_restfull_api_santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfullApiSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
